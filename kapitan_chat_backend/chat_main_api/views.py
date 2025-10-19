532a3dbfb2f581975d03e0f467b95031bbb8ac7f
Azimkin <hayavim2009@gmail.com>
2025-10-19 22:39:40 +0200

    Add views (test)

--- kapitan_chat_backend/chat_main_api/views.py
+++ kapitan_chat_backend/chat_main_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Message, Chat, Attachment
+from .serializers import MessageSerializer, ChatSerializer, AttachmentSerializer
+
 
 # Create your views here.
+class MessageView(ModelViewSet):
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
+
+
+class ChatView(ModelViewSet):
+    serializer_class = ChatSerializer
+    queryset = Chat.objects.all()
+
+
+class AttachmentView(ModelViewSet):
+    serializer_class = AttachmentSerializer
+    queryset = Attachment.objects.all()
