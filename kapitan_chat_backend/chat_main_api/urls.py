c7a763010730d091af592aab33093ab0c11b7fe9
Azimkin <hayavim2009@gmail.com>
2025-10-19 22:40:09 +0200

    Add router with urls

--- kapitan_chat_backend/chat_main_api/urls.py
+++ kapitan_chat_backend/chat_main_api/urls.py
@@ -1,4 +1,10 @@
+from rest_framework.routers import DefaultRouter
 
+from .views import ChatView, AttachmentView, MessageView
 
+router = DefaultRouter()
+router.register(r'attachment', AttachmentView, basename='attachment')
+router.register(r'message', MessageView, basename='message')
+router.register(r'', ChatView, basename='chat')
 
-urlpatterns = []
+urlpatterns = router.urls
