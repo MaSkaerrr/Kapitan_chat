c044896612b76f9a0020a966c3ac05dadf12b45c
Azimkin <hayavim2009@gmail.com>
2025-10-19 20:14:10 +0200

    Base model classes WIP

--- kapitan_chat_backend/chat_main_api/models.py
+++ kapitan_chat_backend/chat_main_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
+class Attachment(models.Model):
+    id = models.AutoField(primary_key=True)
+
+
