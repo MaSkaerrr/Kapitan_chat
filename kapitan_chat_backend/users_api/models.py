e0864e34f5c85ebd38b7c2b5f9c3deebbfcfeaf2
Azimkin <hayavim2009@gmail.com>
2025-10-19 13:30:06 +0200

    Add /me endpoint

--- kapitan_chat_backend/users_api/models.py
+++ kapitan_chat_backend/users_api/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=256, blank=True)
-    phone_number = models.CharField(max_length=11, blank=True)
+    phone_number = models.CharField(max_length=11, blank=False, unique=True)
     # TODO: profile_pic = models.CharField
