2e787e49857bd47c520715d119bda1121ef01673
Azimkin <hayavim2009@gmail.com>
2025-10-20 22:26:42 +0200

    Add auth & some comments

--- kapitan_chat_backend/chat_main_api/views.py
+++ kapitan_chat_backend/chat_main_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Message, Chat, Attachment
@@ -8,15 +9,26 @@ from .serializers import MessageSerializer, ChatSerializer, AttachmentSerializer
 
 # Create your views here.
 class MessageView(ModelViewSet):
+    """
+    Пайплайн публікації повідомлення:
+    Якщо повідомлення з вкладеним файлом ітд
+        1. завантажити файл на сервіс для файлів, та отримати хеш
+        2. завантажити attachment на сервер та отримати його id
+        3. опублікувати повідомлення
+    Якщо вкладення нема - просто завантажити повідомлення на сервер
+    """
+    permission_classes = [IsAuthenticated]
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
 
 
 class ChatView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = ChatSerializer
     queryset = Chat.objects.all()
 
 
 class AttachmentView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = AttachmentSerializer
     queryset = Attachment.objects.all()
